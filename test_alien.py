d765f6b114ac05442351d98ae9f90adedb1dae70
chen-yuhong <59632334+chen-yuhong@users.noreply.github.com>
2022-12-21 21:14:40 -0800

    Update test_alien.py

--- test_alien.py
+++ test_alien.py
@@ -1,6 +1,6 @@
 import unittest
-import sys 
-sys.path.append("..") 
+import sys
+sys.path.insert(0, sys.path[0][:-4])
 from MDS_Big_Adventure.character.alien import alien
 
 class TestAlien(unittest.TestCase):
