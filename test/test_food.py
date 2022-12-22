d81743b5a630c369670a881030c04dd9ab842841
chen-yuhong <59632334+chen-yuhong@users.noreply.github.com>
2022-12-21 21:15:36 -0800

    Update and rename test_food.py to test/test_food.py

--- test/test_food.py
+++ test/test_food.py
@@ -1,6 +1,6 @@
 import unittest
-import sys 
-sys.path.append("..") 
+import sys
+sys.path.insert(0, sys.path[0][:-4])
 from MDS_Big_Adventure.item.food import item
 
 class TestItem(unittest.TestCase):
